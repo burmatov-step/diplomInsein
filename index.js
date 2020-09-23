e0a9e94c16df829840c9ced421afb92da3af4114
burmatov-step <stepaburmatov@mail.ru>
2020-09-23 16:36:27 +0300

    end

--- index.js
+++ index.js
@@ -78,11 +78,51 @@ const toggleMenu = () => {
 
 toggleMenu();
 
+// Отправка формы
+
 const feedback = () => {
-  const feedbackPhone = document.querySelectorAll("[name='phone']");
+  const feedbackPhone = document.querySelectorAll("[name='phone']"),
+    formAll = document.querySelectorAll("form");
 
   feedbackPhone.forEach((e) => {
-    maskPhone(e)
+    maskPhone(e);
+  });
+// функция запроса
+  const postData = (body) => {
+    return fetch("server.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+      body: JSON.stringify(body),
+    });
+  };
+
+
+  formAll.forEach((item) => {
+
+    // обработчик на форму
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const check = e.target.querySelector("[type='checkbox']");
+      if (check.checked) {
+        const formData = new FormData(item);
+        let body = {};
+        formData.forEach((val, key) => {
+          body[key] = val;
+        });
+        postData(body)
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error("status network not 200");
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+
+    });
   });
 };
 
